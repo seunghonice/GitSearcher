fd34d09840dde5f20310181563fe3e836cd5ede4
Seungho Hong <seunghonice@gmail.com>
2021-11-24 22:43:50 +0900

    feat: 검색바 검색 기능 동작
    - 스크롤 내려서 더 불러오기
    - 뷰 설정
    - 뷰모델 에러 수정 (vm, binding.vm 이름 다르게하기)

--- app/src/main/java/com/hongsyong/gitsearcher/SearcherActivity.kt
+++ app/src/main/java/com/hongsyong/gitsearcher/SearcherActivity.kt
@@ -12,16 +12,99 @@ class SearcherActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearcherBinding
     private lateinit var vm: SearcherViewModel
 
+    private lateinit var resultAdapter: ResultAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_searcher)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_searcher)
-        binding.vm = ViewModelProvider(
+        binding.viewmodel = ViewModelProvider(
             this,
             ViewModelFactory.getInstance()
         ).get(SearcherViewModel::class.java)
-        vm = binding.vm as SearcherViewModel
+        vm = binding.viewmodel as SearcherViewModel
 
+        viewSetting()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun viewSetting() {
+        binding.run {
+
+            // 검색바 작성 중 엔터
+            etSearchBar.setOnKeyListener { _, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) { // 엔터
+                    search()
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+            // 검색바 검색버튼 클릭
+            etSearchBar.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) // 검색버튼 클릭
+                    return@setOnEditorActionListener search()
+                return@setOnEditorActionListener false
+            }
+            // 텍스트 입력 시 동작
+            etSearchBar.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    // 검색어 삭제버튼 표시
+                    ivCancel.visibility = if (s?.isEmpty() == true) View.GONE else View.VISIBLE
+                }
+            })
+
+            // 검색 이미지 버튼 클릭
+            ivSearch.setOnClickListener {
+                search()
+            }
+
+            // 검색어 삭제 버튼 클릭
+            ivCancel.setOnClickListener {
+                etSearchBar.text.clear()
+                vm.noResult.set(false)
+                etSearchBar.requestFocus()
+            }
+
+            // 결과 리스트
+            resultAdapter = ResultAdapter(vm.results.value, this@SearcherActivity)
+            rvResults.adapter = resultAdapter
+            rvResults.layoutManager = LinearLayoutManager(application)
+            rvResults.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val totalItemCount = recyclerView.adapter!!.itemCount - 1
+                    if (!recyclerView.canScrollVertically(1) && !vm.processing.get()) {
+                        // 마지막 아이템이 아닐 때에만 수행 -> 다 불러왔으면 수행하지 않도록
+                        if (totalItemCount + 1 < vm.maxCount.get()) {
+                            search(vm.page.get() + 1)
+                        }
+                    }
+                }
+            })
+
+            // 결괏값이 업데이트되면 adapter 갱신
+            vm.results.observe(this@SearcherActivity, {
+                resultAdapter.results = it
+                resultAdapter.notifyDataSetChanged()
+            })
+        }
+    }
+
+    private fun search(page: Int = 1): Boolean {
+        binding.run {
+            if (etSearchBar.text.isEmpty()) {
+                Toast.makeText(this@SearcherActivity, "검색어를 입력해주세요 !", Toast.LENGTH_SHORT).show()
+                etSearchBar.requestFocus()
+                return false
+            }
+
+            // search 시작!
+            vm.page.set(page)
+            vm.searchRepositories(etSearchBar.text.toString())
+            etSearchBar.clearFocus()
+        }
+        return true
     }
 }
